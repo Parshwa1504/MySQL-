5fdd9c46f5a15caf2625afd7816d94e1610e2dc2
Parshwa1504 <parshwagandhi0403@gmail.com>
2024-09-12 21:42:18 -0400

    Update SQL_1.sql

--- SQL_1.sql
+++ SQL_1.sql
@@ -1,1 +1,5 @@
 CREATE DATABASE IF NOT EXISTS Sales;
+-- here create database is for creating a database. if not exist is for to verify that a database with the same name exists already. and sales is a database name.
+-- We can use CREATE SCHEMA IF NOT EXISTS Sales ; aslo for creating a database named sales.
+USE Sales;
+-- USE Sales; query is to we can select the existing sales database in MySQL.
