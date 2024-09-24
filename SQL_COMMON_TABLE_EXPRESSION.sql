236aa50115e303515ac8cd2177af46f35d35414c
Parshwa1504 <parshwagandhi0403@gmail.com>
2024-09-24 18:38:55 -0400

    Update SQL_COMMON_TABLE_EXPRESSION.sql

--- SQL_COMMON_TABLE_EXPRESSION.sql
+++ SQL_COMMON_TABLE_EXPRESSION.sql
@@ -6,7 +6,7 @@ A COMMON TABLE EXPRESSION IS A TOOL FOR OBTAINING SUCH TEMPORARY RESULT SETS THA
 
 SYNTAX : 
 
-WITH cte_name ( SELECT ......... FROM .......)
+WITH cte_name AS  ( SELECT ......... FROM .......)
 SELECT
 ...........
 FROM 
@@ -66,4 +66,247 @@ JOIN employees e ON s.emp_no = e.emp_no
 AND e.gender = 'F'
 CROSS JOIN
 cte c;
+
+-- WE CAN ALSO USE COUNT() FUNCTION IN PLACE OF SUM() FUNCTION TO GET THE NO_OF_FEMALE_salaries_above_avg :
+
+WITH cte AS (SELECT AVG(s.salary) AS avg_salary FROM salaries s)
+SELECT 
+SUM(CASE WHEN s.salary > c.avg_salary THEN 1 ELSE 0 END) AS no_of_female_salaries_above_avg_with_SUM ,
+COUNT(CASE WHEN s.salary > c.avg_salary THEN s.salary ELSE NULL END) AS no_of_female_salaries_above_avg_with_COUNT,
+COUNT(s.salary) AS total_no_of_salary 
+FROM 
+salaries s 
+JOIN employees e ON s.emp_no = e.emp_no 
+AND e.gender = 'F'
+CROSS JOIN
+cte c;
+
+-- SO AS WE CAN SEE ABOVE CTE CAN BE REUSED MULTIPLE TIMES WITHIN A MYSQL STATEMENT .
+
+/*
+Use a CTE (a Common Table Expression) and a SUM() function in the SELECT statement in a query to find out
+ how many male employees have never signed a contract with a salary value higher than or equal to the all-time company salary average.
+*/
+
+WITH cte_male AS ( SELECT AVG(s.salary) AS avg_salary FROM salaries s )
+SELECT 
+SUM(CASE WHEN s.salary < cm.avg_salary THEN 1 ELSE 0 END) AS no_of_male_salaries_below_avg_with_SUM ,
+COUNT(s.salary) AS no_of_male_salaries
+FROM
+salaries s JOIN employees e on s.emp_no = e.emp_no 
+AND e.gender = 'M'
+CROSS JOIN
+cte_male cm;
+
+/*
+Use a CTE (a Common Table Expression) and (at least one) COUNT() function in the SELECT statement of a query to find out
+ how many male employees have never signed a contract with a salary value higher than or equal to the all-time company salary average.
+*/
+
+WITH cte_male AS ( SELECT AVG(s.salary) AS avg_salary FROM salaries s )
+SELECT 
+COUNT(CASE WHEN s.salary < cm.avg_salary THEN s.salary ELSE NULL END) AS no_of_male_salaries_below_avg_with_COUNT ,
+COUNT(s.salary) AS no_of_male_salaries
+FROM
+salaries s JOIN employees e on s.emp_no = e.emp_no 
+AND e.gender = 'M'
+CROSS JOIN
+cte_male cm;
+
+/*
+Use MySQL joins (and don’t use a Common Table Expression) in a query to find out
+how many male employees have never signed a contract with a salary value higher than or equal to the all-time company salary average
+ (i.e. to obtain the same result as in the previous exercise).
+*/
+
+SELECT 
+COUNT(CASE WHEN s.salary < cm.avg_salary THEN s.salary ELSE NULL END) AS no_of_male_salaries_below_avg_with_COUNT ,
+COUNT(s.salary) AS no_of_male_salaries
+FROM
+salaries s JOIN employees e on s.emp_no = e.emp_no 
+AND e.gender = 'M'
+JOIN
+( SELECT AVG(s.salary) AS avg_salary FROM salaries s ) cm;
+
+/*
+Use a cross join in a query to find out how many male employees have never signed a contract
+ with a salary value higher than or equal to the all-time company salary average (i.e. to obtain the same result as in the previous exercise).
+*/
+
+SELECT 
+COUNT(CASE WHEN s.salary < cm.avg_salary THEN s.salary ELSE NULL END) AS no_of_male_salaries_below_avg_with_COUNT ,
+COUNT(s.salary) AS no_of_male_salaries
+FROM
+salaries s JOIN employees e on s.emp_no = e.emp_no 
+AND e.gender = 'M'
+CROSS JOIN
+( SELECT AVG(s.salary) AS avg_salary FROM salaries s ) cm;
+
+/*
+USING MULTIPLE SUBCLAUSE IN A WITH CLAUSE : COMMON TABLE EXPRESSIONS CAN HAVE MULTIPLE SUBCLAUSES .
+WE CAN NOT HAVE MULTIPLE WITH CLASUSE WITHIN THE SAME MYSQL QUERY . WE CAN USE NUMEROUS SUBCLAUSES .
+
+SYNTAX: 
+
+WITH cte_name_1  AS ( SELECT ......... FROM .......) ,
+cte_name_2 AS (SELECT .......... FROM .............)
+SELECT
+...........
+FROM 
+..........
+cte_name_1 c1 
+JOIN
+cte_name_2 c2 ;
+*/
+
+-- HOW MANY FEMALE EMPLOYEES HIGHEST CONTRACT SALARY VALUES WERE HIGHER THAN ALL TIME COMPANY SALARY AVERAGE ACROSS ALL GENDERS 
+-- NOTE : THE HIGHEST SALARY VALUE OF AN EMPLOYEE MAY NOT HAVE BERN HIGHER THAN THE ALL TIME COMPANY AVERAGE SO WE WILL ONLY BE INTERESTED IN THE REST OF THE CASES OF OUR TASK.
+-- SUBCLAUSE 1 : A CTE COMPUTING THE ALL TIME AVERAGE 
+-- SUBCLAUSE 2 : A CTE TO OBTAIN A LIST CONTAINING THE HIGHEST CONTRACT SALARY VALUES OF ALL FEMALE EMPLOYEES.
+
+-- FIRST WE WILL WRITE THE CODE WHICH WE WILL ADD IN THE TWO SUBCLAUSE :
+
+SELECT AVG(s.salary) AS avg_salary FROM salaries s;
+
+SELECT 
+    s.emp_no, MAX(s.salary) AS Max_salary
+FROM
+    salaries s
+GROUP BY s.emp_no;
+
+-- NOW WE WILL ADD ABOVE TWO CODES IN TWO SUBCLAUSE : 
+
+WITH cte1 AS (SELECT AVG(salary) AS avg_salary FROM salaries ), 
+cte2 AS 
+(SELECT
+ s.emp_no , MAX(s.salary) AS Max_salary FROM salaries s 
+ JOIN employees e ON s.emp_no = e.emp_no 
+ AND e.gender = 'F'
+ GROUP BY s.emp_no
+)
+SELECT 
+SUM(CASE WHEN c2.Max_salary > c1.avg_salary THEN 1 ELSE 0 END) AS no_of_female_max_salary_contracts_above_avg_salary,
+COUNT(e.emp_no) AS Total_no_of_female_salary_contracts
+FROM employees e
+JOIN 
+cte2 c2 ON c2.emp_no = e.emp_no
+CROSS JOIN
+cte1 c1;
+
+/*
+HOW MANY FEMALE EMPLOYEES HIGHEST CONTRACT SALARY VALUES WERE HIGHER THAN THE ALL TIME COMPANY SALARY AVERAGE ( ACROSS ALL GENDERS ) 
+IN ABOVE CODE WE GOT THIS OUTPUT USING SUM() FUNCTION BUT WE CAN ALSO GET THIS USING COUNT() FUNCTION HERE IS THE CODE BELOW: 
+*/
+
+WITH cte1 AS (SELECT AVG(salary) AS avg_salary FROM salaries ), 
+cte2 AS 
+(SELECT
+ s.emp_no , MAX(s.salary) AS Max_salary FROM salaries s 
+ JOIN employees e ON s.emp_no = e.emp_no 
+ AND e.gender = 'F'
+ GROUP BY s.emp_no
+)
+SELECT 
+COUNT(CASE WHEN c2.Max_salary > c1.avg_salary THEN c2.Max_salary ELSE NULL END) AS no_of_female_max_salary_contracts_above_avg_salary_with_count,
+COUNT(e.emp_no) AS Total_no_of_female_salary_contracts
+FROM employees e
+JOIN 
+cte2 c2 ON c2.emp_no = e.emp_no
+CROSS JOIN
+cte1 c1;
+
+-- WE CAN TRY TO OBTAIN RELEVANT PERCENTAGE VALUE FOR THE ABOVE CODE AND ALSO WE CAN DISPLAY IT BETTER USING ROUND() FUNCTION WE CAN ADD ANY STRING TO A VLAUE BY CONCAT() FUNCTION  :
+
+WITH cte1 AS (SELECT AVG(salary) AS avg_salary FROM salaries ), 
+cte2 AS 
+(SELECT
+ s.emp_no , MAX(s.salary) AS Max_salary FROM salaries s 
+ JOIN employees e ON s.emp_no = e.emp_no 
+ AND e.gender = 'F'
+ GROUP BY s.emp_no
+)
+SELECT 
+COUNT(CASE WHEN c2.Max_salary > c1.avg_salary THEN c2.Max_salary ELSE NULL END) AS no_of_female_max_salary_contracts_above_avg_salary_with_count,
+COUNT(e.emp_no) AS Total_no_of_female_salary_contracts,
+CONCAT(ROUND((COUNT(CASE WHEN c2.Max_salary > c1.avg_salary THEN c2.Max_salary ELSE NULL END) /(COUNT(e.emp_no)) )*100 , 2) , '%') AS '% percentage'
+FROM employees e
+JOIN 
+cte2 c2 ON c2.emp_no = e.emp_no
+CROSS JOIN
+cte1 c1;
+
+/*
+Use two common table expressions and a SUM() function in the SELECT statement of a query to obtain the number of male employees
+ whose highest salaries have been below the all-time average.
+*/
+
+WITH cte_1 AS (	 SELECT AVG(s.salary) AS avg_salary FROM salaries s) ,
+ cte_2 AS
+ (SELECT s.emp_no , MAX(s.salary) AS max_salary FROM salaries s JOIN employees e ON s.emp_no = e.emp_no AND e.gender = 'M' GROUP BY s.emp_no)
+ SELECT 
+ SUM(CASE WHEN c2.max_salary < c1.avg_salary THEN 1 ELSE 0 END),
+ COUNT(e.emp_no)
+ FROM employees e
+ JOIN cte_2 c2 ON e.emp_no = c2.emp_no 
+ CROSS JOIN
+ cte_1 c1 ;
+
+/*
+Use two common table expressions and a COUNT() function in the SELECT statement of a query to obtain the number of male employees
+ whose highest salaries have been below the all-time average.
+*/
+
+WITH cte_1 AS (	 SELECT AVG(s.salary) AS avg_salary FROM salaries s) ,
+ cte_2 AS
+ (SELECT s.emp_no , MAX(s.salary) AS max_salary FROM salaries s JOIN employees e ON s.emp_no = e.emp_no AND e.gender = 'M' GROUP BY s.emp_no)
+ SELECT 
+ COUNT(CASE WHEN c2.max_salary < c1.avg_salary THEN c2.max_salary ELSE NULL END),
+ COUNT(e.emp_no)
+ FROM employees e
+ JOIN cte_2 c2 ON e.emp_no = c2.emp_no 
+ CROSS JOIN
+ cte_1 c1 ;
+ 
+ /*
+ Does the result from the previous exercise change
+ if you used the Common Table Expression (CTE) for the male employees' highest salaries in a FROM clause, as opposed to in a join?
+ 
+ ANS : NO THE RESULT DOES NOT CHANGE AS WE CAN DIRECTLY USE FROM RATHER THAN USING EMPLOYEES TABLE TO JOIN C2 BUT FOR THE GOOD PRACTICE WE SHOULD ALWAYS JOIN 
+ CTE WITH DIFFERENT TABLES IN THE DATABASE  
+ */
+ 
+ /*
+ REFERRING TO COMMON TABLE EXPRESSION IN A WITH CLAUSE : WE CAN USE MULTIPLE CTES IN A QUERY REFERENCING THEM NUMEROUS TIME .
+ WE CAN REFER TO A CTE DEFINED EARLIER WITHIN A WITH CLAUSE . WE CAN NOT REFER TO A ONE THAT HAS BEEN DEFINED AFTER .
+ */
+
+-- task : RETRIEVE THE HIGHEST CONTRACT SALARY VALUES OF ALL EMPLOYEES HIRED IN 2000 OR LATER : 
+
+ WITH cte_1 AS (SELECT * FROM employees e WHERE e.hire_date >= '2000-01-01'),
+ cte_2 AS (
+SELECT c1.emp_no , MAX(s.salary) AS max_salary FROM salaries s JOIN cte_1 c1 ON s.emp_no = c1.emp_no GROUP BY s.emp_no ) -- WE HAVE JOINED C1 AND SALARIES TABLE IN THIS STEP
+SELECT * FROM cte_2 c2;
+
+-- THE SECOND CTE IN A QUERY CAN REFERENCE THE FIRST ONR BUT THIS IS NOT TRUE VICA VERSA.
+
+/*
+Considering the salary contracts signed by female employees in the company, how many have been signed for a value below the average? 
+Store the output in a column named no_f_salaries_below_avg. 
+In a second column named total_no_of_salary_contracts, provide the total number of contracts signed by all employees in the company.
+Use the salary column from the salaries table and the gender column from the employees table. Match the two tables on the employee number column (emp_no).
+*/
+
+
+WITH cte AS (
+    SELECT AVG(salary) AS avg_salary FROM salaries
+),
+cte2 AS (
+    SELECT COUNT(salary) AS total_no_of_salary_contracts FROM salaries
+)
+SELECT
+    SUM(CASE WHEN s.salary < c.avg_salary THEN 1 ELSE 0 END) AS no_f_salaries_below_avg,
+    (SELECT total_no_of_salary_contracts FROM cte2) AS total_no_of_salary_contracts
+FROM salaries s
+JOIN employees e ON s.emp_no = e.emp_no AND e.gender = 'F'
+JOIN cte c;
 
